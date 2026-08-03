da4f4f268d6e95c88d55c0346f2fcb8303cd3654
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-03 20:42:35 +0000

    Created stg table for google sheets accounts.

---
+++ models/staging/stg_google_sheets__accounts.sql
@@ -0,0 +1,10 @@
+select
+    _fivetran_synced, 
+    _row as account_id, 
+    account as account_name, 
+    cast( coalesce( beginning_balance, 0 ) as numeric ) as beginning_balance, 
+    cast( coalesce( current_balance, 0 ) as numeric ) as current_balance, 
+    account_type, 
+    account_category
+from {{ source('src_google_sheets', 'accounts') }}
+where account is not null
