9336734716db21c0046b485feb218b0f07d67648
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-04 17:07:31 +0000

    Created staging layer for savings goals details table.

---
+++ models/staging/stg_google_sheets__savings_goals_details.sql
@@ -0,0 +1,8 @@
+select
+    _fivetran_synced, 
+    _row as goals_details_id, 
+    account as account_name, 
+    goal_name, 
+    item as goal_item, 
+    cast( amount as numeric) as goal_amount
+from {{ source('src_google_sheets', 'savings_goals_details') }}
