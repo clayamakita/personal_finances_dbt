968a7732b0446b8353c1f8b254d5f1a862660d5f
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-05 21:46:07 +0000

    Updated dim_accounts with current balance column coming from transactions table.

--- models/marts/dim_accounts.sql
+++ models/marts/dim_accounts.sql
@@ -1,6 +1,33 @@
-select 
-  account_name,
-  account_type, 
-  account_category, 
-  beginning_balance
-from {{ ref('stg_google_sheets__accounts') }}
+with accounts as (
+
+    select * from {{ ref('stg_google_sheets__accounts') }}
+
+), 
+transactions as (
+
+    select * from {{ ref('stg_google_sheets__cashflow_transactions') }}
+
+),
+transactions_total as (
+
+    select
+        account_name,
+        sum(transaction_amount) as total_amount
+    from transactions
+    group by account_name
+
+),
+final as (
+
+    select 
+        accounts.account_name,
+        accounts.account_type, 
+        accounts.account_category, 
+        accounts.beginning_balance, 
+        accounts.beginning_balance + coalesce( transactions_total.total_amount, 0 ) as current_balance
+    from accounts
+    left join transactions_total 
+    on transactions_total.account_name = accounts.account_name
+
+)
+select * from final
