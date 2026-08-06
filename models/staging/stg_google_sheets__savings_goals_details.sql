fb60b87c9bf5820c738d27fc05163f048016a383
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-06 21:40:27 +0000

    Refactored and standardized staging model for savings goals details table.

--- models/staging/stg_google_sheets__savings_goals_details.sql
+++ models/staging/stg_google_sheets__savings_goals_details.sql
@@ -1,8 +1,18 @@
-select
-    _fivetran_synced, 
-    _row as goals_details_id, 
-    account as account_name, 
-    goal_name, 
-    item as goal_item, 
-    cast( amount as numeric) as goal_amount
-from {{ source('google_sheets', 'savings_goals_details') }}
+with source as (
+
+    select * from {{ source('google_sheets', 'savings_goals_details') }}
+), 
+
+cleaned_columns as (
+
+    select
+        goal_detail_id, 
+        account as account_name, 
+        goal_name, 
+        item as goal_item, 
+        cast( amount as numeric) as goal_amount, 
+        _fivetran_synced as _loaded_at
+    from source
+)
+
+select * from cleaned_columns
