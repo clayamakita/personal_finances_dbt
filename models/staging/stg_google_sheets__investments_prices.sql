9cbf025e9bd29ca25ac4f7d53fc454b7ed75d105
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-06 21:24:22 +0000

    Refactored and standardized staging model for investments prices table.

--- models/staging/stg_google_sheets__investments_prices.sql
+++ models/staging/stg_google_sheets__investments_prices.sql
@@ -1,9 +1,21 @@
-select
-    _fivetran_synced, 
-    _row as investments_price_id, 
-    ticker as investment_ticker, 
-    investment_name, 
-    live_price as latest_price, 
-    last_trade_time, 
-    time_delay
-from {{ source('google_sheets', 'investments_price') }}
+with source as (
+
+    select * from {{ source('google_sheets', 'investments_price') }}
+
+), 
+
+cleaned_columns as (
+
+    select
+        investment_price_id, 
+        ticker as investment_ticker, 
+        investment_name, 
+        cast( live_price as numeric ) as latest_price, 
+        time_delay, 
+        last_trade_time as latest_trade_time, 
+        _fivetran_synced as _loaded_at 
+    from source
+
+)
+
+select * from cleaned_columns
