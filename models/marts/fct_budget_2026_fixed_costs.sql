090c65ce6c119e7f2765c88ec6463eccdf95aa14
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-06 23:07:27 +0000

    Updated primary key logic for fct_budget_2026_fixed_costs model.

--- models/marts/fct_budget_2026_fixed_costs.sql
+++ models/marts/fct_budget_2026_fixed_costs.sql
@@ -7,13 +7,22 @@ with budget as (
 budget_aggregated as (
 
     select 
-        {{ dbt_utils.generate_surrogate_key(['budget_category', 'budget_month']) }} as fct_budget_fixed_costs_id, 
         budget_category, 
         budget_month, 
         sum(budget_amount) as total_budget_amount
     from budget
-    group by 1, 2, 3
+    group by 1, 2
 
+), 
+
+added_key as (
+
+    select 
+        replace( budget_category, ' ', '_' ) || '-' || cast( budget_month as string ) as budget_fixed_costs_fct_id, 
+        budget_category, 
+        budget_month, 
+        total_budget_amount
+    from budget_aggregated
 )
 
-select * from budget_aggregated
+select * from added_key
