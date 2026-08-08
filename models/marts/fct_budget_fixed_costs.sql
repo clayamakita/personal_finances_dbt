747ca239c54ce848949179edc464f36dc91699ee
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-08 20:36:53 +0000

    Removed primary key column being added in the fct_budget_fixed_costs model, since it was already included in the intermediate model.

--- models/marts/fct_budget_fixed_costs.sql
+++ models/marts/fct_budget_fixed_costs.sql
@@ -1,28 +1,17 @@
 with budget as (
 
-    select * from {{ ref('int_budget_2026_fixed_costs_unpivoted') }}
+    select * from {{ ref('int_budget_2026_fixed_costs_agg_category') }}
 
 ),
 
-budget_aggregated as (
+final as (
 
     select 
+        budget_fixed_costs_month_id, 
         budget_category, 
         budget_month, 
-        sum(budget_amount) as total_budget_amount
+        budget_amount
     from budget
-    group by 1, 2
-
-), 
-
-added_key as (
-
-    select 
-        replace( budget_category, ' ', '_' ) || '-' || cast( budget_month as string ) as budget_fixed_costs_fct_id, 
-        budget_category, 
-        budget_month, 
-        total_budget_amount
-    from budget_aggregated
 )
 
-select * from added_key
+select * from final
