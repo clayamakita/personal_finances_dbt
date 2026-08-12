76cab360d6ab8b09990269a7e781e84fb5bb91b8
Clarissa Yamakita <clarissa.yamakita@gmail.com>
2026-08-12 14:35:50 +0000

    Added calculation for annualized return rate to dim_investments.

--- models/marts/dim_investments.sql
+++ models/marts/dim_investments.sql
@@ -30,10 +30,56 @@ current_position as (
         transactions.investment_name, 
         transactions.total_quantity, 
         prices.latest_price, 
-        round( transactions.total_quantity * prices.latest_price, 2 ) as total_value
+        round( transactions.total_quantity * prices.latest_price, 2 ) as current_value
     from aggregated_transactions as transactions 
     left join prices as prices 
     on prices.ticker = transactions.ticker 
+), 
+
+transactions_and_current_position as (
+
+    select 
+        ticker, 
+        safe_cast( total_value as float64 ) as amount, 
+        event_date
+    from transactions 
+
+    union all 
+
+    select 
+        ticker, 
+        safe_cast( current_value as float64 ) as amount, 
+        current_date() as event_date
+    from current_position
+
+),
+
+calculated_annualized_return as (
+
+    select 
+        ticker, 
+        round(
+            `{{ target.project }}.{{ target.dataset }}.xirr`(
+                array_agg( amount order by event_date ),
+                array_agg( cast( event_date as string) order by event_date )
+                ), 4 )
+        as annualized_return_rate 
+    from transactions_and_current_position
+    group by ticker
+),
+
+added_annualized_return_to_current_position as (
+
+    select
+        position.ticker, 
+        position.investment_name, 
+        position.total_quantity, 
+        position.latest_price, 
+        position.current_value, 
+        rate.annualized_return_rate
+    from current_position as position 
+    left join calculated_annualized_return as rate 
+    on rate.ticker = position.ticker
 )
 
-select * from current_position
+select * from added_annualized_return_to_current_position
